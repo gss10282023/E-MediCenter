b762cc36ef6a69d50b3f9b2cd41874c51787c071
hzou2291 <hzou2291@uni.sydney.edu.au>
2023-08-18 15:26:15 +1000

    Basic frame

---
+++ Caregiver/Caregiver/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def runoob(request):
+    context = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'runoob.html', context)
